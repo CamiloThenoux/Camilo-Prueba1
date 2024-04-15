85a83835e36fd4265b9d27f2340aedab841a2bf1
CamiloThenoux <camilo.thenoux@usm.cl>
2024-04-15 01:24:48 -0400

    Subo repositorio prueba

---
+++ Sesion 6/js/scripts.js
@@ -0,0 +1,12 @@
+alert("Hola Mundo 1!")
+function saludar(){
+    alert("Hola Mundo 3!")
+}
+function colorear(){
+    /*console.log ("SI")*/
+    var h1 = document.getElementById("saludo")
+    h1.innerHTML = "Polla!"
+    h1.classList.add("fondo")
+
+}
+
